96606a492d9325cfee2301c74ec11ff5bc19c0f7
johnpauladal <zhulinmeix643@163.com>
2021-04-30 16:51:05 +0800

    problem 4

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -1,6 +1,12 @@
 /* 3. Reverse String
 Define function: reverseStr(someStr)
 Reverse and return the String. */
+let joinArray = "";
 function reverseStr(someStr) {
-  
+    let splitString = someStr.split(""); 
+    let reverseArray = splitString.reverse(); 
+    joinArray = reverseArray.join("");
+    // console.log(joinArray)
+    return joinArray
 }
+// reverseStr("Hello")
--- problems/04-factorial/04-factorial.js
+++ problems/04-factorial/04-factorial.js
@@ -2,5 +2,9 @@
 Define function: factorial(someNum)
 Use recursion to compute and return the factorial of someNum.  */
 function factorial(sumNum) {
-  
+    if(sumNum == 0) {
+        return 1
+    } else {
+        return sumNum * factorial(sumNum - 1);
+        }
 }
