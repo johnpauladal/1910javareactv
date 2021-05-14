8f34128f6f7a140209fb6357f63cd61394d4c500
johnpauladal <zhulinmeix643@163.com>
2021-05-14 08:36:15 +0800

    problem 10

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,20 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+
+function Car() {
+  this.name = "";
+  this.age = 0;
+}
+
+var Car = {
+  name: "Pontiac",
+  age: 33
+};
+
+class Car {
+  constructor(name, age) {
+    this.name = "";
+    this.age = 0;
+  }
+}
