38d08f934fe8a124a45d7783e191802f616f8a94
johnpauladal <zhulinmeix643@163.com>
2021-05-16 02:02:55 +0800

    problem 11

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -3,5 +3,19 @@ Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
 function spliceElement(someArr, index) {
-  
+    console.log(someArr);
+    
+    for(let i = index; i < someArr.length; i++) {
+        someArr[i] = someArr[i+1];
+    }
+    console.log(someArr);
+    
+    someArr = someArr.pop();
+    console.log(someArr);
+    
 }
+finalArr = spliceElement([5,4,3,2,1], 3);
+console.log(finalArr);
+
+
+
